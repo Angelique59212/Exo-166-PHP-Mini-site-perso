524369bf4a25e7d779bb304f1fb1974771d9eb77
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-31 19:24:48 +0100

    resolve problem message

--- pages/contact.php
+++ pages/contact.php
@@ -9,19 +9,9 @@
         <input type="email" name="mail" id="mail" placeholder="Entrez votre mail">
 
         <label for="message"></label>
-        <textarea name="message" id="message" cols="30" rows="10" maxlength="500" placeholder="Votre messsage" required></textarea>
+        <textarea name="message" id="message" cols="30" rows="10" maxlength="250" placeholder="Votre messsage" required></textarea>
 
         <button type="submit" name="submit">Envoyer</button>
     </form>
 
 </section>
-
-<?php
-if (isset($_SESSION['mail'])) {
-    if ($_SESSION['mail'] === "mail-success") {
-        echo "Le mail est bien envoyé ";
-    }
-    else {
-        echo "Erreur lors de l'envoi";
-    }
-}?>
--- public/save.php
+++ public/save.php
@@ -4,6 +4,20 @@ $name = trim(strip_tags($_POST['name']));
 $message = trim(strip_tags($_POST['message']));
 $userMail = trim(strip_tags($_POST['mail']));
 
+//if (isset($_POST['submit']) && isset($_POST['mail']) && isset($_POST['message'])) {
+//    $to = trim(strip_tags($_POST['mail']));
+//    $message = trim(strip_tags($_POST['message']));
+//    $subject = "Email a envoyé";
+//    if (filter_var($to, FILTER_VALIDATE_EMAIL)) {
+//        if (strlen($message <= 500)) {
+//            if (mail($to, $subject, $message)) {
+//                echo "Email envoyé avec succès vers $to ...";
+//            } else {
+//                echo "Échec de l'envoi de l'email...";
+//            }
+//        }
+//    }
+//}
 if (isset($_POST['submit']) && isset($_POST['name']) && isset($_POST['message'])) {
     $files[] = [$name,$message];
 }
@@ -15,11 +29,11 @@ if (isset($_POST['mail'])) {
         'X-Mailer' => 'PHP/' . phpversion()
     );
     if (filter_var($userMail, FILTER_VALIDATE_EMAIL)) {
-        if (strlen($message <= 500)) {
+        if (strlen($message <= 250)) {
             if (mail($to, $subject, $message, $headers, $userMail)) {
-               $_SESSION['mail'] = "mail-success";
+                echo "Le mail est bien envoyé ";
             } else {
-                $_SESSION['mail'] =  "error";
+                echo "Erreur lors de l'envoi";
             }
         }
     }
@@ -27,4 +41,4 @@ if (isset($_POST['mail'])) {
 
 $jsonMessage = file_put_contents("../data/last_message.json", json_encode($files));
 
-header('Location: /../public/?page=contact');
+//header('Location: /../public/?page=contact');
